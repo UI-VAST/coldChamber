55cb61a99e67e40cde92d8f7fc69dd6e38c1bc3b
malachiRivkin <Malachiriv@gmail.com>
2021-09-23 23:57:26 -0700

    third RTD address added, RTD address finder script added

--- tempSensor.ino
+++ tempSensor.ino
@@ -10,20 +10,30 @@ OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
 
+
 // Addresses of 3 DS18B20s
 uint8_t sensor1[8] = { 0x28, 0x2C, 0xC2, 0x99, 0x09, 0x00, 0x00, 0x88 };
 uint8_t sensor2[8] = { 0x28, 0x75, 0xF2, 0xA7, 0x08, 0x00, 0x00, 0x42 };
+uint8_t sensor3[8] = { 0x28, 0x54, 0x4E, 0x98, 0x09, 0x00, 0x00, 0xD4 };
 
+
+int time;
 
 void setup(void)
 {
   Serial.begin(9600);
   sensors.begin();
+ 
 }
 
 void loop(void)
 {
+  time = millis()/1000;
   sensors.requestTemperatures();
+
+  Serial.print("Time: ");
+  Serial.print(time);
+  Serial.print("  ,   ");
   
   Serial.print("Temp1: ");
   printTemperature(sensor1);
@@ -31,6 +41,11 @@ void loop(void)
   
   Serial.print("Temp2: ");
   printTemperature(sensor2);
+  Serial.print("  ,   ");
+
+  Serial.print("Temp3: ");
+  printTemperature(sensor3);
+  
   Serial.println();
   
   delay(1000);
